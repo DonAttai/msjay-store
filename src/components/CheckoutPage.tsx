ead82d771d1dee249952520a7a759e91b198a5c8
John Attai Yinusa <donattai1032@gmail.com>
2023-12-21 14:11:25 +0100

    modify user-store

--- src/components/CheckoutPage.tsx
+++ src/components/CheckoutPage.tsx
@@ -1,7 +1,7 @@
 export const CheckoutPage = () => {
   return (
     <div className="min-h-[calc(100vh-128px)]">
-      Still working on th add page....
+      Still working on the page....
     </div>
   );
 };
