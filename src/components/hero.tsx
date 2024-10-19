6402cc6d19affe029590d897f15bc8dd6a299308
John Attai Yinusa <donattai1032@gmail.com>
2024-10-19 07:05:50 +0100

    change h1 text to 3xl

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,9 +13,10 @@ export const Hero = () => {
     >
       <div className="overlay bg-black bg-opacity-50 h-full w-full flex items-center justify-center">
         <div className="text-center">
-          <h1 className="text-white text-4xl font-bold">
+          <h1 className="text-white text-3xl font-bold">
             Welcome to Ms Jay Store
           </h1>
+          <p>Your Best Online Store Destination!</p>
           <a href="#products">
             <Button className="mt-6 px-6 py-3 text-lg font-bold bg-green-500 text-white rounded-lg hover:bg-green-700">
               Shop Now
