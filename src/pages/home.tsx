8d529de35a66f33f8ccd7dc57a2a0c1758a3d001
John Attai Yinusa <donattai1032@gmail.com>
2024-12-31 07:25:09 +0100

    change background image link

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,13 +5,13 @@ export function Home() {
     <section
       className="relative h-screen bg-cover bg-center"
       style={{
-        backgroundImage: `url('/images/hero-image.jpg')`,
+        backgroundImage: `url('https://res.cloudinary.com/dte2inrui/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1735625754/hero-image_h1wldd.jpg')`,
         backgroundSize: "contain",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="overlay bg-black bg-opacity-50 h-full w-full flex items-center justify-center">
+      <div className="bg-black bg-opacity-50 h-full w-full flex items-center justify-center">
         <div className="text-center">
           <h1 className="text-[#F5F5F5] text-3xl font-bold ">
             Welcome to Ms Jay Store!
